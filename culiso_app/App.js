fbb6af33688a2974eb82d3e4cc5f64a0a83f6a60
fghjklvmbn <dsapoi881@gmail.com>
2024-08-22 14:11:36 +0900

    코드 수정

--- culiso_app/App.js
+++ culiso_app/App.js
@@ -9,21 +9,37 @@ import FindPasswordScreen from './app/screens/FindPasswordScreen';
 import SignupSuccessScreen from './app/screens/SignupSuccessScreen';
 import IdResultScreen from './app/screens/IdResultScreen';
 import PasswordResultScreen from './app/screens/PasswordResultScreen';
+import { SafeAreaView, View, Text } from 'react-native';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="SignupSuccess" component={SignupSuccessScreen} />
-        <Stack.Screen name="FindId" component={FindIdScreen} />
-        <Stack.Screen name="FindPassword" component={FindPasswordScreen} />
-        <Stack.Screen name="IdResult" component={IdResultScreen} />
-        <Stack.Screen name="PasswordResult" component={PasswordResultScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen
+            name='Login'
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    
   );
 }
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Login">
+//         <Stack.Screen name="Login" component={LoginScreen} />
+//         <Stack.Screen name="Signup" component={SignupScreen} />
+//         <Stack.Screen name="SignupSuccess" component={SignupSuccessScreen} />
+//         <Stack.Screen name="FindId" component={FindIdScreen} />
+//         <Stack.Screen name="FindPassword" component={FindPasswordScreen} />
+//         <Stack.Screen name="IdResult" component={IdResultScreen} />
+//         <Stack.Screen name="PasswordResult" component={PasswordResultScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
